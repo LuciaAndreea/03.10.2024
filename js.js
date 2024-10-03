3d05c1a8dc34debe7059a73623e93dca59d5d6b9
Lucia <luciaandreeaursu@yahoo.com>
2024-10-03 18:37:09 +0300

    add if-else

---
+++ js.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+let userAge = 18;
+let legalAge = 18;
+let isLegal = userAge >=legalAge;
+
+if(isLegal){
+    console.log("Utilizatorul poate vota")
+}else{
+    console.log("Utilizatorul nu poate vota")
+}
+
