8d44f5594a5450883243ab1a0e8b433e6c8a9ed5
Lucia <luciaandreeaursu@yahoo.com>
2024-10-03 18:58:06 +0300

    add ternar operator and else if

--- js.js
+++ js.js
@@ -10,3 +10,31 @@ if(isLegal){
     console.log("Utilizatorul nu poate vota")
 }
 
+//operatorul ternar
+
+let userEnteredPassword = "123pass";
+let correctPassword = "123pass";
+
+let accessMesssage = userEnteredPassword == correctPassword ? "Access granted" : " Access denied";  
+
+console.log("User has access: ",accessMesssage);
+
+//if...else if...else
+
+let actualScore = 100;
+let minimumScore = 50;
+let averageScore = 70;
+let maximumScore = 100;
+
+if(actualScore ===100){
+    console.log("Congratulations! Your score is perfect.")
+}else if(actualScore < maximumScore && actualScore>averageScore){
+    console.log("Congratulations! You score is great.")
+}else if(actualScore<=averageScore && actualScore>=minimumScore){
+    console.log("Congratulations! You got a passing score")
+}else if(actualScore < minimumScore){
+    console.log("We are sorry, but your score is too low.Try again.")
+}else{
+    console.log("Your score could not be verified")
+}
+
