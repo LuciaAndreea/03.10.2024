a9200ccccc01feddc9cca2fd5e5358406faed23a
Lucia <luciaandreeaursu@yahoo.com>
2024-10-03 19:04:05 +0300

    add switch

--- js.js
+++ js.js
@@ -38,3 +38,31 @@ if(actualScore ===100){
     console.log("Your score could not be verified")
 }
 
+//switch
+
+let weekDay = 5;
+switch(weekDay){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid value");
+}
